e0154bca514be70b0e203b59abc46693e00a7949
Anthony Truskinger <anthony.truskinger@gmail.com>
2016-10-23 18:17:46 +1000

    Added .NET4.6 Class Library for test
    Could not get xunit working on .NET Core

--- test/SunCalcDotNet.Test/Class1.cs
+++ test/SunCalcDotNet.Test/Class1.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SunCalcDotNet.Test
 {
-    // This project can output the Class library as a NuGet Package.
-    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    using Xunit;
+
     public class Class1
     {
-        public Class1()
+        [Fact]
+        public void PassingTest()
         {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        [Fact]
+        public void FailingTest()
+        {
+            Assert.Equal(5, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
